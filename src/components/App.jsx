0e0d14d0d25ddf9f66a4a8795ebf9561037045bb
Osyotr1 <92748388+Osyotr1@users.noreply.github.com>
2022-11-01 21:40:00 +0200

    abc

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,35 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+import React, { Component } from "react";
+import Feedbacks from './Feedbacks/Feedbacks';
+
+class App extends Component {
+  state = {
+    
+  }
+
+  handleBtnGood = () => {
+    console.log('Good pressed');
+  }
+
+  handleBtnNeutral = () => {
+    console.log('Neutral pressed');
+  }
+
+  handleBtnBad = () => {
+    console.log('Bad pressed');
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>Please leave feedback</h2>
+        <button type="button" onClick={this.handleBtnGood}>Good</button>
+        <button type="button" onClick={this.handleBtnNeutral}>Neutral</button>
+        <button type="button" onClick={this.handleBtnBad}>Bad</button>
+        <h1>Statistics</h1>
+      <Feedbacks />
+      </div>
+    );
+  }
 };
+
+export default App;
