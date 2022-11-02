28b5b53becd065ebf21038c26a608087ce82fa07
Osyotr1 <92748388+Osyotr1@users.noreply.github.com>
2022-11-02 20:04:51 +0200

    startup

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,21 +3,48 @@ import Feedbacks from './Feedbacks/Feedbacks';
 
 class App extends Component {
   state = {
-    
-  }
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
 
   handleBtnGood = () => {
-    console.log('Good pressed');
+    this.setState((prevState) => {
+      return {
+        good: prevState.good + 1
+      }
+    });
   }
 
   handleBtnNeutral = () => {
-    console.log('Neutral pressed');
+    this.setState((prevState) => {
+      return {
+        neutral: prevState.neutral + 1
+      }
+    });
   }
 
   handleBtnBad = () => {
-    console.log('Bad pressed');
+    this.setState((prevState) => {
+      return {
+        bad: prevState.bad + 1
+      }
+    });
   }
 
+  countTotalFeedback = () => {
+    const { good, neutral, bad } = this.state;
+    return good + neutral + bad;
+  }
+
+  countPositiveFeedbackPercentage = () => {
+    const total = this.countTotalFeedback();
+    return Math.round((this.state.good * 100) / total);
+  };
+
+
+ 
   render() {
     return (
       <div>
@@ -26,6 +53,11 @@ class App extends Component {
         <button type="button" onClick={this.handleBtnNeutral}>Neutral</button>
         <button type="button" onClick={this.handleBtnBad}>Bad</button>
         <h1>Statistics</h1>
+        <span>Good: {this.state.good}<br></br></span>
+        <span>Neutral: {this.state.neutral}<br></br></span>
+        <span>Bad: {this.state.bad}<br></br></span>
+        <span>Total: {this.countTotalFeedback()}<br></br></span>
+        <span>Positive feedback: {this.countPositiveFeedbackPercentage()}%</span>
       <Feedbacks />
       </div>
     );
